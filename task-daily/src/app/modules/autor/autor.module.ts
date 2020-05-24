3a46d276a4fc899d8861e95e7ac44307c116f7e6
mramoss <moi6ramoss@gmail.com>
2020-05-24 12:29:58 -0400

    Creando  componestes formularios libro y autor

--- task-daily/src/app/modules/autor/autor.module.ts
+++ task-daily/src/app/modules/autor/autor.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AutorRoutingModule } from './autor-routing.module';
+import { FormAutorComponent } from './form-autor/form-autor.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FormAutorComponent],
   imports: [
     CommonModule,
     AutorRoutingModule
---
+++ task-daily/src/app/moudles/libro/form-libro/form-libro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-libro',
+  templateUrl: './form-libro.component.html',
+  styleUrls: ['./form-libro.component.scss']
+})
+export class FormLibroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
