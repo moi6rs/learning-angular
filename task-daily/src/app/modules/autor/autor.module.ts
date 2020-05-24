148d7e78fb23356e8bbe97b6122be82d9909d590
mramoss <moi6ramoss@gmail.com>
2020-05-24 12:20:54 -0400

    Agregamos Modulos libro y autor

---
+++ task-daily/src/app/modules/autor/autor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AutorRoutingModule } from './autor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AutorRoutingModule
+  ]
+})
+export class AutorModule { }
---
+++ task-daily/src/app/modules/libro/libro.module.ts
@@ -0,0 +1,12 @@
+import { AutorRoutingModule } from '../autor/autor-routing.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AutorRoutingModule
+  ]
+})
+export class LibroModule { }
